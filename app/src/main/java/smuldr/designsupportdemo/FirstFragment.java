b59ad6d27bfa9798cd64ba517f8c79928a405c48
Steven Mulder <steven.mulder@gmail.com>
2015-06-01 21:48:15 +0200

    Added basic NavigationView demo

---
+++ app/src/main/java/smuldr/designsupportdemo/FirstFragment.java
@@ -0,0 +1,17 @@
+package smuldr.designsupportdemo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+}
--- app/src/main/java/smuldr/designsupportdemo/MainActivity.java
+++ app/src/main/java/smuldr/designsupportdemo/MainActivity.java
@@ -1,38 +1,131 @@
 package smuldr.designsupportdemo;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+public class MainActivity extends AppCompatActivity implements
+    NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity {
+  private static final long DRAWER_CLOSE_DELAY_MS = 350;
+  private static final String NAV_ITEM_ID = "navItemId";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  private final FirstFragment mFirstFragment = new FirstFragment();
+  private final SecondFragment mSecondFragment = new SecondFragment();
+  private final Handler mDrawerActionHandler = new Handler();
+  private DrawerLayout mDrawerLayout;
+  private ActionBarDrawerToggle mDrawerToggle;
+  private int mNavItemId;
+
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    // load saved navigation state if present
+    if (null == savedInstanceState) {
+      mNavItemId = R.id.drawer_item_1;
+    } else {
+      mNavItemId = savedInstanceState.getInt(NAV_ITEM_ID);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    // listen for navigation events
+    NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+    navigationView.setNavigationItemSelectedListener(this);
+
+    // select the correct nav menu item
+    navigationView.getMenu().findItem(mNavItemId).setChecked(true);
+
+    // set up the hamburger icon to open and close the drawer
+    mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open,
+        R.string.close);
+    mDrawerLayout.setDrawerListener(mDrawerToggle);
+    mDrawerToggle.syncState();
+
+    navigate(mNavItemId);
+  }
+
+  /**
+   * Performs the actual navigation logic, updating the main content fragment.
+   */
+  private void navigate(final int itemId) {
+    switch (itemId) {
+      case R.id.drawer_item_1:
+        getFragmentManager()
+            .beginTransaction()
+            .replace(R.id.content, mFirstFragment)
+            .commit();
+        break;
+      case R.id.drawer_item_2:
+        getFragmentManager()
+            .beginTransaction()
+            .replace(R.id.content, mSecondFragment)
+            .commit();
+        break;
+      default:
+        // ignore
+        break;
     }
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+  /**
+   * Handles clicks on the navigation menu.
+   */
+  @Override
+  public boolean onNavigationItemSelected(final MenuItem menuItem) {
+    // update highlighted item in the navigation menu
+    menuItem.setChecked(true);
+    mNavItemId = menuItem.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    // allow some time after closing the drawer before performing real navigation
+    // so the user can see what is happening
+    mDrawerLayout.closeDrawer(GravityCompat.START);
+    mDrawerActionHandler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        navigate(menuItem.getItemId());
+      }
+    }, DRAWER_CLOSE_DELAY_MS);
+    return true;
+  }
 
-        return super.onOptionsItemSelected(item);
+  @Override
+  public void onConfigurationChanged(final Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    mDrawerToggle.onConfigurationChanged(newConfig);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(final MenuItem item) {
+    if (item.getItemId() == android.support.v7.appcompat.R.id.home) {
+      return mDrawerToggle.onOptionsItemSelected(item);
     }
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public void onBackPressed() {
+    if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+      mDrawerLayout.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  protected void onSaveInstanceState(final Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putInt(NAV_ITEM_ID, mNavItemId);
+  }
 }
